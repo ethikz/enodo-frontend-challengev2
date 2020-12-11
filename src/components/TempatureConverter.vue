600576a27371862314f04f01c2af5be11dde03c9
Josh Christensen <josh.christensen2@gmail.com>
2020-12-11 10:12:18 -0500

    feat(temp-converter): add cel - far and far - c converter

--- src/components/TempatureConverter.vue
+++ src/components/TempatureConverter.vue
@@ -7,15 +7,55 @@
     <p>
       The formula for converting a temperature from celsius to fahrenheit is <strong>C = (F - 32) * (5/9)</strong> and the reverse is <strong>F = C * (9/5) + 32.</strong>
     </p>
+
+    <div>
+      <label for="celsius">Celsius</label>
+      <el-input
+        id="celsius"
+        placeholder="0"
+        v-model="c"
+        @change="convertC()"
+        @input="convertC()"
+        type="number">
+      </el-input>
+    </div>
+    
+    <div>
+      <label for="fahrenheit">Fahrenheit</label>
+      <el-input
+        id="fahrenheit"
+        placeholder="0"
+        v-model="f"
+        @change="convertF()"
+        @input="convertF()"
+        type="number">
+      </el-input>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'tempature-converter'
+  name: 'tempature-converter',
+
+  data() {
+    return {
+      c: 0,
+      f: 0
+    }
+  },
+
+  methods: {
+    convertC() {
+      this.f = Math.round( ( this.c * 9 / 5 ) + 32 );
+    },
+
+    convertF() {
+      this.c = Math.round( ( this.f - 32 ) * 5 / 9 );
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
