7f3114cfe313979a0137fe8f1c6c436604729c33
Josh Christensen <josh.christensen2@gmail.com>
2020-12-11 09:43:16 -0500

    refactor(counter): add whitespace to code

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -3,9 +3,17 @@
     <h2>Counter</h2>
     <p>Build a component containing a label or read-only input and a button. The initial value of the label/input should be `0` and every button click should increase that value by `1`</p>
 
-    <h2 class="title">{{ counter }}</h2>
-    <el-button @click="incrementCounter">Count Up</el-button>
-    <el-button @click="decrementCounter">Count Down</el-button>
+    <h2 class="title">
+      {{ counter }}
+    </h2>
+    
+    <el-button @click="incrementCounter">
+      Count Up
+    </el-button>
+    
+    <el-button @click="decrementCounter">
+      Count Down
+    </el-button>
   </div>
 </template>
 
